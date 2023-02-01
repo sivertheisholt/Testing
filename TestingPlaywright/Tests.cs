5b5c6c0cd2c15f6b4c895ef44c2d60d28f869524
Sivert Barsgård Heisholt <sivert.heisholt@bouvet.no>
2023-02-01 15:24:30 +0100

    Added basic test

---
+++ TestingPlaywright/Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace TestingPlaywright
+{
+    public class Tests : PageTest
+    {
+
+        [Test]
+        public async Task OperatorAuthorization()
+        {
+            var chrome = Playwright.Chromium;
+            var browser = await chrome.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync($"https://vg.no");
+
+            await Expect(page).ToHaveURLAsync("https://www.vg.no/");
+
+            await browser.CloseAsync();
+        }
+
+    }
+}
