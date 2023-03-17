405b7d2ee61dbff317fa3e0000fd75ee550dc0a6
Sivert Barsgård Heisholt <sivert.heisholt@bouvet.no>
2023-03-17 09:03:42 +0100

    test

---
+++ FunctionAppTest/httppostpdf/index.js
@@ -0,0 +1,9 @@
+const json = false;
+
+module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+  // You can call and await an async method here
+  return {
+    body: "Hello, world!",
+  };
+};
